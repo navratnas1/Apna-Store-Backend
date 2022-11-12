a1832c08597f415d826425a11f925fdd225434e9
Navratna Sharma <navsharma26@gmail.com>
2022-11-12 13:37:05 +0530

    first commit

---
+++ src/index.server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const env = require('dotenv').config(); 
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const userRoutes = require('../src/routes/user');
+const adminRoutes = require('../src/routes/admin');
+const categoryRoutes = require('../src/routes/category')
+const productRoutes = require('../src/routes/product');
+const cartRoutes = require('../src/routes/cart');
+const initialDataRoutes = require('../src/routes/initialData');
+const pageRoutes = require('../src/routes/page');
+const addressRoutes = require('../src/routes/address');
+const orderRoutes = require('../src/routes/order');
+const adminOrderRoutes = require('../src/routes/admin.order');
+
+const dbURI = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.kmwnp.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+mongoose.connect(
+    dbURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(() => {
+    console.log('Database is connected...!');
+})
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/category', categoryRoutes);
+app.use('/product', productRoutes);
+app.use('/user', cartRoutes);
+app.use('/admin', initialDataRoutes);
+app.use('/page', pageRoutes);
+app.use('/user', addressRoutes);
+app.use('/order', orderRoutes);
+app.use('/order', adminOrderRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
---
+++ src/routes/category.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { requireSignin, adminMiddleware } = require('../common-middleware');
+const router = express.Router();
+const { createCategory, getCategories, updateCategories, deleteCategories } = require('../controller/category');
+
+const multer = require('multer');
+const shortid = require('shortid');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(path.dirname(__dirname), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, shortid.generate() + '-' + file.originalname)
+    }
+});
+const upload = multer({ storage: storage });
+
+router.post('/create', requireSignin, adminMiddleware, upload.single('categoryImage'), createCategory);
+router.get('/getCategories', getCategories);
+router.post('/update', upload.array('categoryImage'), updateCategories);
+router.post('/delete', deleteCategories);
+
+
+module.exports = router;
